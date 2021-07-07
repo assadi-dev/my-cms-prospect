67c60645578377398feec615a8f397b18f7c05bb
Assadi <assadi.halifa@gmail.com>
2021-07-07 12:41:57 +0200

    make it better

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const path = require("path");
+const https = require("https");
+const fs = require("fs");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+const sslServer = https.createServer(
+  {
+    key: "",
+    cert: "",
+  },
+  app
+);
 
 app.use(express.static("client/build"));
 
