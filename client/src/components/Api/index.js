86cbe636e8800d81c025836f500682155f3d80d0
Assadi <assadi.halifa@gmail.com>
2021-07-21 12:48:49 +0200

    fix language

--- client/src/components/Api/index.js
+++ client/src/components/Api/index.js
@@ -2,5 +2,5 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export default axios.create({
-  baseURL: "https://memo-prospection-api.herokuapp.com/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
