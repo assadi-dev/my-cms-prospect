9175e9d2828fb6c68a5673aec9f1584c3142c7a2
Assadi <assadi.halifa@gmail.com>
2021-08-20 22:48:40 +0200

    Update index.js

--- client/src/components/Api/index.js
+++ client/src/components/Api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import Cookies from "js-cookie";
 
+console.log(process.env.REACT_APP_API_URL);
 export default axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
